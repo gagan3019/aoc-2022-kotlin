cd98449f3d19d124fd56f672533186449c7fd3ec
Ggaandeep Singh <gagandeep3019@gmail.com>
2022-12-05 22:26:40 +0100

    Added kotlin performance library

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,15 @@
 plugins {
     kotlin("jvm") version "1.7.22"
+    id("org.jetbrains.kotlinx.benchmark") version "0.4.4"
 }
 
 repositories {
     mavenCentral()
 }
 
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-benchmark-runtime:0.4.6")
+}
 tasks {
     sourceSets {
         main {
